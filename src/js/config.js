65b31ff91b250187932f04b00d95933c20392a64
Deyan9 <didkoz99@abv.bg>
2023-12-05 13:29:12 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b7fc6260-3e8a-40bb-bfc4-a8b244ddcbe6';
+export const MODAL_CLOSE_SEC = 2.5;
